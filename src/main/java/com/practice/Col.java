1c3b4b3a7b44171e2713faf48c44dc5899e6a640
Rahul Kumar <rahul@thoughtnirvana.com>
2017-05-22 11:23:34 +0530

    Print columns

---
+++ src/main/java/com/practice/Col.java
@@ -0,0 +1,26 @@
+package com.practice;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+
+public class Col {
+    public static void main(String args[]) throws Exception {
+        String inputFile = null;
+        if (args.length > 1)
+            inputFile = args[1];
+        InputStream is = System.in;
+        if (inputFile != null)
+            is = new FileInputStream(inputFile);
+        ANTLRInputStream input = new ANTLRInputStream(is);
+        RowsLexer lexer = new RowsLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        int col = Integer.valueOf(args[0]);
+        RowsParser parser = new RowsParser(tokens, col);
+        parser.setBuildParseTree(false);
+        parser.file();
+    }
+}
